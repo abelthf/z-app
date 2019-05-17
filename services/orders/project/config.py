cf6a43d093917c34adfa11dfa2d3ba0a3d414814
Fredy Abel Huanca Torres <abel.huanca@upeu.edu.pe>
2019-05-17 08:35:28 -0500

    sqlite

--- services/orders/project/config.py
+++ services/orders/project/config.py
@@ -1,21 +1,28 @@
 # services/orders/project/config.py
+import os
+
 
 
 class BaseConfig:
     """Configuración báse"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # nuevo
 
 
 class DevelopmentConfig(BaseConfig):
     """Configuración de desarrollo"""
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # nuevo
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL' + db_path)  # nuevo
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL' + db_path)  # nuevo
     pass
 
 
 class TestingConfig(BaseConfig):
     """Configuración de prueba"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')  # nuevo
 
 
 class ProductionConfig(BaseConfig):
     """Configuración de producción"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # nuevo
